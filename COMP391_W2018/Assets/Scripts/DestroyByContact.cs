73c8a3caf8b9f42c23778a3ae00d77a58c68d8db
crownclowny <35781653+crownclowny@users.noreply.github.com>
2018-03-07 14:18:30 -0500

    added new components
     asteroid and explosions animation

---
+++ COMP391_W2018/Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour {
+
+    public GameObject explosionAsteroid;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	
+	void onTriggerEnter2D (Collider2D other)
+    {
+        if (other.tag == "Boundary")
+        {
+            return;
+            //Debug.Log("DestroyedByContact");
+        }
+
+        Instantiate(explosionAsteroid, this.transform.position, this.transform.rotation);
+        Destroy(other.gameObject);//Destroy the other thing(Lazer)
+        Destroy(this.gameObject);//Destroying the asteroid
+        
+	}
+}
